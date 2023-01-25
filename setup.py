d3e8b90919330b8aad76b3682bcd8629884a9b12
takato86 <okudo@nii.ac.jp>
2023-01-25 13:00:57 +0900

    add package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_m2s',
-      packages=["gym_m2s", "gym_m2s.robotics"],
+      packages=["gym_m2s", "gym_m2s.robotics", "gym_m2s.robotics.fetch"],
       version='0.0.1',
       install_requires=['gym==0.21.0']  # And any other dependencies foo needs
 )
